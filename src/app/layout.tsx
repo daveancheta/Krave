489c4832786001a3b37e7e1195179c1b374a1bd4
Heaven Dave Ancheta <daveancheta8@gmail.com>
2026-01-31 12:36:45 +0800

    chore: remove unused metadata for cleaner layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,39 +7,6 @@ const inter = Inter({
   subsets: ['latin'],
 });
 
-export const metadata: Metadata = {
-  title: {
-    template: "%s | Krave",
-    default: "Krave",
-  },
-  description:
-    "Krave is a ready-made AI chatbot assistant built with Next.js, featuring Gemini, OpenAI, and Claude API integration with a UI starter kit.",
-  
-  keywords: [
-    "Krave", "AI Chatbot", "Next.js", "Gemini API", "OpenAI API", "Claude API", "UI Starter", "Chatbot Guide"
-  ],
-
-  authors: [{ name: "Heaven Dave Ancheta" }],
-
-  openGraph: {
-    title: "Krave – AI Chatbot Assistant with Next.js",
-    description:
-      "Step-by-step guide to build AI chatbots using Gemini, OpenAI, and Claude APIs with a Next.js UI starter kit.",
-    url: "https://krave-docs.vercel.app",
-    siteName: "Krave Docs",
-    images: [
-      {
-        url: "/home.png",
-        width: 1200,
-        height: 630,
-        alt: "Krave Docs – AI Chatbot",
-      },
-    ],
-    type: "website",
-  },
-};
-
-
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
