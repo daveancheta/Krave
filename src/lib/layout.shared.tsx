1dc5fb7469816d3d12af9fa6819cee3752350583
Heaven Dave Ancheta <daveancheta8@gmail.com>
2026-01-31 10:38:05 +0800

    refactor: code structure for improved readability and add icon

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,9 +1,15 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import { Brain } from 'lucide-react';
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: 'Krave',
+      title: (
+        <div className="flex items-center gap-2">
+          <Brain />
+          <span className="font-semibold">Krave</span>
+        </div>
+      ),
     },
     githubUrl: 'https://github.com/daveancheta/Krave',
   };
