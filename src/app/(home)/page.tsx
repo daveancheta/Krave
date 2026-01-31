bf4705d5710977f29a83ff6f979e8cf8dc18bd23
Heaven Dave Ancheta <daveancheta8@gmail.com>
2026-01-31 11:59:51 +0800

    feat: add chatbot UI components and enhance homepage
    - Introduced ChatContainer component for chatbot interface.
    - Added Avatar, Button, and InputGroup components for UI consistency.
    - Implemented HeroVideoDialog for video playback on the homepage.
    - Enhanced homepage layout with new components and improved styling.
    - Updated global CSS for better theming and responsiveness.
    - Added utility functions for class name management.
    - Included new dependencies for UI components and animations.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,55 @@
+import ChatContainer from '@/components/ai/chat-container';
+import { Button } from '@/components/ui/button';
+import { HeroVideoDialog } from '@/components/ui/hero-video-dialog';
+import { AnimatedSpan, Terminal, TypingAnimation } from '@/components/ui/terminal';
 import Link from 'next/link';
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col justify-center text-center flex-1">
-      <h1>
+    <div className="wrapper flex flex-col flex-1 items-center justify-center text-center gap-6 px-4">
+      <h1 className="text-3xl md:text-5xl font-black tracking-tight text-transparent 
+      bg-clip-text bg-linear-to-r dark:from-gray-500 dark:to-white from-gray-500 to-black">
+        Start Your Chatbot Journey with Next.js
       </h1>
+
+      <p className="max-w-xl text-sm md:text-base text-muted-foreground">
+        Learn how to connect Gemini, OpenAI, and other AI APIs in a Next.js app.
+        Krave guides you through backend logic, frontend setup, and chatbot flows
+        with a ready-made UI foundation.
+      </p>
+
+      <div className="flex gap-4 mt-4">
+        <Button asChild>
+          <Link href="/docs">
+            Get Started
+          </Link>
+        </Button>
+
+        <Button variant="outline" asChild>
+          <Link href="/components">
+            View Components
+          </Link>
+        </Button>
+      </div>
+      <div className='flex flex-row gap-2'>
+        <div className="relative sm:w-200">
+          <HeroVideoDialog
+            className="block dark:hidden"
+            animationStyle="top-in-bottom-out"
+            videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+            thumbnailSrc="https://startup-template-sage.vercel.app/hero-light.png"
+            thumbnailAlt="Hero Video"
+          />
+          <HeroVideoDialog
+            className="hidden dark:block"
+            animationStyle="top-in-bottom-out"
+            videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+            thumbnailSrc="https://startup-template-sage.vercel.app/hero-dark.png"
+            thumbnailAlt="Hero Video"
+          />
+        </div>
+        <ChatContainer />
+      </div>
     </div>
   );
 }
---
+++ src/components/ai/chat-container.tsx
@@ -0,0 +1,61 @@
+"use client"
+import { Send, X } from 'lucide-react'
+import { InputGroup, InputGroupAddon, InputGroupButton, InputGroupText, InputGroupTextarea } from '../ui/input-group'
+import React, { useEffect, useRef, useState } from 'react'
+import { cn } from '@/lib/utils';
+import { Avatar, AvatarImage } from '../ui/avatar';
+
+function ChatContainer() {
+    const [prompt, setPrompt] = useState<string>("")
+
+    return (
+        <div className='sm:relative bg-background border
+        rounded-md sm:min-w-100 max-w-100 flex flex-col w-screen text-start sm:block hidden'>
+            <div className='flex flex-col h-full'>
+                <div className='flex items-center justify-between px-4 py-2 border-b-2'>
+                    <div className='flex flex-row items-center gap-2'>
+                        <Avatar>
+                            <AvatarImage src="https://github.com/shadcn.png" />
+                        </Avatar>
+                        <div className='flex flex-col gap-1'>
+                            <span className='font-bold'>Krave</span>
+                            <div className='flex flex-row items-center gap-1'>
+                                <div className='w-2 h-2 bg-green-500 rounded-full'></div>
+                                <span className='text-xs'>Active Now</span>
+                            </div>
+                        </div>
+                    </div>
+                    <button><X /></button>
+                </div>
+
+                <div className='flex-1 overflow-y-auto space-y-4 p-2 scrollable-div'>
+                    <div className='flex flex-col items-start gap-2'>
+                        <div className='bg-black dark:bg-white rounded-lg p-4 w-fit max-w-60'>
+                            <span className='text-white dark:text-black text-sm'>Hello! I’m Krave, your friendly AI chatbot assistant. I’m here to answer questions, provide guidance, and chat with you anytime. Ask me anything, and let’s explore and learn together!</span>
+                        </div>
+                    </div>
+
+                </div>
+
+                <InputGroup className='rounded-t-none'>
+                    <InputGroupTextarea
+                        id="block-end-textarea"
+                        placeholder="Aa"
+                        maxLength={100}
+                        onChange={(e) => setPrompt(e.target.value)}
+                        value={prompt}
+                    />
+                    <InputGroupAddon align="block-end">
+                        <InputGroupText>{prompt.length}/100</InputGroupText>
+                        <InputGroupButton variant="default" size="sm"
+                            className="ml-auto" disabled={!prompt}>
+                            <Send />
+                        </InputGroupButton>
+                    </InputGroupAddon>
+                </InputGroup>
+            </div >
+        </div >
+    )
+}
+
+export default ChatContainer
